a684a786b0945f3a064fd9a7bc3af8bd211f9b54
gempurbayu <aji123zz@gmail.com>
2022-06-28 13:19:04 +0700

    rev

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,16 @@
 module.exports = {
   content: ["./public/**/*.{html,js}"],
+   mode: 'jit',
+
+   // These paths are just examples, customize them to match your project structure
+
+   purge: [
+
+     './public/**/*.html',
+
+     './src/**/*.{js,jsx,ts,tsx,vue}',
+
+   ],
   theme: {
     extend: {
       screens: {
