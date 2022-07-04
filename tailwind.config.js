4d26754eb0366f908d10698f30b6381398188efc
Gempur Bayu Aji <aji123zz@gmail.com>
2022-07-04 13:07:25 +0700

    revisi arrow

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,17 +13,7 @@ module.exports = {
    ],
   theme: {
     extend: {
-      screens: {
-        'tablet': '440px',
-        // => @media (min-width: 640px) { ... }
-  
-        'laptop': '1024px',
-        // => @media (min-width: 1024px) { ... }
-  
-        'desktop': '1280px',
-        // => @media (min-width: 1280px) { ... }
-      },
-    },
+    }
     
   },
   plugins: [],
